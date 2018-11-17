bc7e89bed23f7569ee56f7bf6a3f5effa4f76da1
Bin <827433149@qq.com>
2018-11-17 10:12:44 +0800

    add db conection status

--- db/index.js
+++ db/index.js
@@ -3,6 +3,9 @@ const mysql = require("mysql");
 
 let connection = mysql.createConnection(config)
 
-connection.connect()
+connection.connect((err) => {
+  if (err) throw new Error("数据库连接失败，检查数据库配置")
+  
+})
 
 module.exports = connection;
